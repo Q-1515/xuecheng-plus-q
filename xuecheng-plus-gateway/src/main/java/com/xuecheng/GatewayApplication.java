874683d6fe737e3c7006a9d204cf2fe7f5c669e3
Q <1515639932@qq.com>
2024-03-26 14:11:08 +0800

    gateway网关搭建

---
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * packageName com.xuecheng
+ *
+ * @author Q
+ * @version JDK 8
+ * @className GatewayApplication
+ * @date 2024/3/26 14:02
+ * @description 网关启动类
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
